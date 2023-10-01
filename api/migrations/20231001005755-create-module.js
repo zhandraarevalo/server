9012d023011f29c1ee4d934b5251d373433cfa76
Zhandra Arévalo <zhandra.arevalo@kavak.com>
2023-10-01 04:04:01 -0600

    adding active column to module table

--- api/migrations/20231001005755-create-module.js
+++ api/migrations/20231001005755-create-module.js
@@ -5,6 +5,7 @@ module.exports = `
     tag varchar(255) not null,
     icon varchar(255) not null,
     route varchar(255) not null,
+    active boolean default true,
     created_at datetime not null default current_timestamp,
     updated_at datetime not null default current_timestamp on update current_timestamp,
     primary key (id)
