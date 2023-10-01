e22961e5b1d4681f21bd16c0e5df34e91b57d2f2
Zhandra Arévalo <zhandra.arevalo@kavak.com>
2023-10-01 01:54:02 -0600

    removing unnecesary blank spaces

--- scripts/create_migration.sh
+++ scripts/create_migration.sh
@@ -9,7 +9,7 @@ operation=""
 
 if [[ $1 == $var_create ]]
 then
-  operation="\ncreate table if not exists $2 (\nid int not null auto_increment, \n\ncreated_at datetime not null default current_timestamp, \nupdated_at datetime not null default current_timestamp on update current_timestamp, \nprimary key (id)\n)\n"
+  operation="\ncreate table if not exists $2 (\nid int not null auto_increment,\n\ncreated_at datetime not null default current_timestamp,\nupdated_at datetime not null default current_timestamp on update current_timestamp,\nprimary key (id)\n)\n"
 elif [[ $1 == $var_update ]]
 then
   fileName="$timestamp-$1-$2-$3"
