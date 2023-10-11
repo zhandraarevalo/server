9be6943ccf65133289d7e94667246e0453119974
Zhandra Arévalo <zhandra.arevalo@kavak.com>
2023-10-10 23:11:10 -0600

    db schema improvements

--- api/migrations/20231004095518-create-backup.js
+++ api/migrations/20231004095518-create-backup.js
@@ -2,8 +2,10 @@ module.exports = `
   create table if not exists backup (
     id int not null auto_increment,
     date date not null,
+    user int not null,
     created_at datetime not null default current_timestamp,
     updated_at datetime not null default current_timestamp on update current_timestamp,
-    primary key (id)
+    primary key (id),
+    constraint fk_bck_user foreign key (user) references user(id)
   )
 `;
